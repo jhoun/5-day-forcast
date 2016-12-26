0132c60c464b97454570eab7215025e69c23d060
jhoun <jayhoun@gmail.com>
2016-12-26 12:40:54 -1000

    created multiple day forcast

--- public/js/client.js
+++ public/js/client.js
@@ -2,10 +2,13 @@
 //Calls when button pressed
 function getWeather(){
 
+var textField = document.getElementById('textfield')
+var newCity = textField.value;
+
   //Request Data from Weather.com
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", fiveDayForcast);
-  oReq.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=Honolulu,us&units=imperial&mode=json&APPID=3e14644f4fc2bdffc2dca11cd62cf631");
+  oReq.open("GET", `http://api.openweathermap.org/data/2.5/forecast?q=${newCity},us&units=imperial&mode=json&APPID=3e14644f4fc2bdffc2dca11cd62cf631`);
   oReq.send();
 
   //Function for oReq
@@ -14,26 +17,49 @@ function getWeather(){
     console.log(data);
     var city = data.city.name;
     var country = data.city.country;
-    var date = data.list[0].dt_txt;
-    var max = data.list[0].main.temp_max;
-    var min = data.list[0].main.temp_min;
 
-    //Creates innHTML Results for City & Country
-    var cityName = document.getElementById('city name');
-    cityName.innerHTML = city + " , " +  country;
+    //Grabs "results Id"
+    var results = document.getElementById('results');
 
-    for (var i = 0; i < data.list.length; i += 5){
-      console.log(data.list[i]);
+    //creates "city" dev
+    var createCity = document.createElement('div');
+    createCity.id = 'city';
+    createCity.innerHTML = city + ", " +  country;
+    results.appendChild(createCity);
+
+    //creates content from results
+    var content = document.createElement('div');
+    content.classList.add('content');
+    results.appendChild(content);
+
+
+    //loops through all the dates
+    for (var i = 0; i < data.list.length; i += 9){
+    var date = data.list[i].dt_txt;
+    var max = data.list[i].main.temp_max;
+    var min = data.list[i].main.temp_min;
+
+    console.log(moment(data.list[i].dt_txt).format());
+
+    var weatherContainer = document.createElement('div');
+    weatherContainer.classList.add('weatherContainer');
+    content.appendChild(weatherContainer);
+
+    var createDate = document.createElement('div');
+    createDate.classList.add('date');
+    createDate.innerHTML = date;
+    weatherContainer.appendChild(createDate);
+
+    var createMax = document.createElement('div');
+    createMax.classList.add('max');
+    createMax.innerHTML = "Max: " + max;
+    weatherContainer.appendChild(createMax);
+
+    var createMin = document.createElement('div');
+    createMin.classList.add('min');
+    createMin.innerHTML = "Min: " + min;
+    weatherContainer.appendChild(createMin);
     }
-
-    var getDate = document.getElementById('date');
-    getDate.innerHTML = date;
-
-    var getMaxTemp = document.getElementById('max');
-    getMaxTemp.innerHTML = "Max: " + max;
-
-    var getMinTemp = document.getElementById('min');
-    getMinTemp.innerHTML = "Min: " + min;
   }
 }
 
